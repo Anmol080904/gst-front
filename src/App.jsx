1127049d0063de884047da2249c5af4121453567
Anmol080904 <anmolchudasma1404@gmail.com>
2025-07-26 20:03:51 +0530

    update2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+// App.js
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import InvoiceForm from './pages/InvoiceForm';
@@ -14,29 +16,31 @@ import Settings from './pages/Settings';
 import PrivateRoute from './pages/PrivateRoute';
 import HomePage from './pages/Home';
 import Signup from './pages/SignUp';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup/>}/>
-        {/* <Route path="/signup" element={<SignUp />}/> */}
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        <Route path="/invoice/create" element={<PrivateRoute><InvoiceForm /></PrivateRoute>} />
-        <Route path="/invoice/list" element={<PrivateRoute><InvoiceList /></PrivateRoute>} />
-        <Route path="/gstr-reports" element={<PrivateRoute><GSTRReport /></PrivateRoute>} />
-        <Route path="/itc-tracker" element={<PrivateRoute><ITCTracker /></PrivateRoute>} />
-        <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
-        <Route path="/monthly-summary" element={<PrivateRoute><MonthlySummary /></PrivateRoute>} />
-        <Route path="/export-center" element={<PrivateRoute><ExportCenter /></PrivateRoute>} />
-        <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
-        <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/invoice/create" element={<PrivateRoute><InvoiceForm /></PrivateRoute>} />
+          <Route path="/invoice/list" element={<PrivateRoute><InvoiceList /></PrivateRoute>} />
+          <Route path="/gstr-reports" element={<PrivateRoute><GSTRReport /></PrivateRoute>} />
+          <Route path="/itc-tracker" element={<PrivateRoute><ITCTracker /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+          <Route path="/monthly-summary" element={<PrivateRoute><MonthlySummary /></PrivateRoute>} />
+          <Route path="/export-center" element={<PrivateRoute><ExportCenter /></PrivateRoute>} />
+          <Route path="/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
+          <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
